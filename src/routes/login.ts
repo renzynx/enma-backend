282ea21630b016d85c4b65c57740a79461f14982
renzynx <10262118-renzynx@users.noreply.gitlab.com>
2022-03-09 14:25:16 +0700

    remove deprecated option

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -3,7 +3,7 @@ import passport from 'passport';
 
 const router = Router();
 
-router.get('/', passport.authenticate('discord', { prompt: 'consent' }));
+router.get('/', passport.authenticate('discord'));
 router.get('/callback', passport.authenticate('discord'), (_req, res) => res.redirect(`${process.env.FRONTEND_DOMAIN}/dashboard`));
 
 export default router;
